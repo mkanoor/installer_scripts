e76cb5b82f5f77dc79e025cd31c41f7a76f6f910
Madhu Kanoor <mkanoor@redhat.com>
2020-06-22 17:06:58 -0400

    Added RHEL version check

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #! /bin/sh
 
+MAJOR_VERSION=`cat /etc/os-release | grep -w VERSION_ID | cut -d= -f2 | tr -d '"' | cut -d. -f1`
+
 subscription-manager register --serverurl https://subscription.rhsm.qa.redhat.com/subscription --username $USERNAME --password $PASSWORD --auto-attach
-subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
-subscription-manager repos --enable rhel-7-server-ansible-2.9-rpms
+if [ "$MAJOR_VERSION" -eq 8 ]
+  subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
+then
+elif [ "$MAJOR_VERSION" -eq 7 ]
+  subscription-manager repos --enable rhel-7-server-ansible-2.9-rpms
+then
+else
+  echo "Unsupported version of RHEL $MAJOR_VERSION"
+  exit 1
+fi
 dnf -y install ansible
 ansible-galaxy install mkanoor.catalog_receptor_installer
 
@@ -23,4 +33,4 @@ update-ca-trust
 dnf config-manager --add-repo=http://dogfood.sat.engineering.redhat.com/pulp/repos/Sat6-CI/QA/Satellite_6_8_with_RHEL7_Server/custom/Satellite_6_8_Composes/Satellite_6_8_RHEL7/
 dnf config-manager --add-repo=http://file.rdu.redhat.com/mkanoor/
 
-./entrypoint.sh
+ansible_playbook /playbooks/install_receptor.yml
