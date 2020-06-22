4be65d7c356bc501b4ea9dcdc0e5110173cd8574
Madhu Kanoor <mkanoor@redhat.com>
2020-06-22 17:17:11 -0400

    check_presence_of_certs

--- install.sh
+++ install.sh
@@ -2,17 +2,23 @@
 
 MAJOR_VERSION=`cat /etc/os-release | grep -w VERSION_ID | cut -d= -f2 | tr -d '"' | cut -d. -f1`
 
+FILE=/etc/pki/consumer/cert.pem
+if [[ ! -f "$FILE" ]]
+then
 subscription-manager register --serverurl https://subscription.rhsm.qa.redhat.com/subscription --username $USERNAME --password $PASSWORD --auto-attach
-if [ "$MAJOR_VERSION" -eq 8 ]
+fi
+
+if [[ "$MAJOR_VERSION" -eq 8 ]]
+then
   subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
+elif [[ "$MAJOR_VERSION" -eq 7 ]]
 then
-elif [ "$MAJOR_VERSION" -eq 7 ]
   subscription-manager repos --enable rhel-7-server-ansible-2.9-rpms
-then
 else
   echo "Unsupported version of RHEL $MAJOR_VERSION"
   exit 1
 fi
+
 dnf -y install ansible
 ansible-galaxy install mkanoor.catalog_receptor_installer
 
@@ -33,4 +39,4 @@ update-ca-trust
 dnf config-manager --add-repo=http://dogfood.sat.engineering.redhat.com/pulp/repos/Sat6-CI/QA/Satellite_6_8_with_RHEL7_Server/custom/Satellite_6_8_Composes/Satellite_6_8_RHEL7/
 dnf config-manager --add-repo=http://file.rdu.redhat.com/mkanoor/
 
-ansible_playbook /playbooks/install_receptor.yml
+ansible-playbook /playbooks/install_receptor.yml
