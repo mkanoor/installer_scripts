88e38a6686d516235108844090e20cc6d3f2798e
Madhu Kanoor <mkanoor@redhat.com>
2020-06-22 14:17:03 -0400

    Use dnf

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 subscription-manager register --serverurl https://subscription.rhsm.qa.redhat.com/subscription --username $USERNAME --password $PASSWORD --auto-attach
 subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
 subscription-manager repos --enable rhel-7-server-ansible-2.9-rpms
-yum -y install ansible
+dnf -y install ansible
 ansible-galaxy install mkanoor.catalog_receptor_installer
 
 # We need the latest python-dateutil package for the Receptor
@@ -20,7 +20,7 @@ update-ca-trust
 #ENV PYTHONPATH /opt/app-root/lib/python3.6/site-packages:$PYTHON_PATH
 
 # Setup RPM repo for the python receptor & catalog plugin
-yum config-manager --add-repo=http://dogfood.sat.engineering.redhat.com/pulp/repos/Sat6-CI/QA/Satellite_6_8_with_RHEL7_Server/custom/Satellite_6_8_Composes/Satellite_6_8_RHEL7/
-yum config-manager --add-repo=http://file.rdu.redhat.com/mkanoor/
+dnf config-manager --add-repo=http://dogfood.sat.engineering.redhat.com/pulp/repos/Sat6-CI/QA/Satellite_6_8_with_RHEL7_Server/custom/Satellite_6_8_Composes/Satellite_6_8_RHEL7/
+dnf config-manager --add-repo=http://file.rdu.redhat.com/mkanoor/
 
 ./entrypoint.sh
